5819dc382a253b7f802707bf86f48d8afa705737
unknown <iagosto.nasc@gmail.com>
2022-09-01 14:21:50 -0300

    Commit-A010

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const Array1 = [22, 01, 03, 02, 09, 11]
+const Array2 = ['Boa tarde']
+const Array3 = [22, 01, 03, 'Boa tarde', true]
+
+console.log(Array1);
+console.log(Array2);
+console.log(Array3);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const Array1 = [22, 01, 03, 02, 09, 11]
+const Array2 = ['Boa tarde', 'Tudo bem?']
+const Array3 = [22, 01, 03, 'Boa tarde', true]
+
+console.log(Array1);
+console.log(Array2);
+console.log(Array3);
+
+console.log(Array1.length, Array2.length, Array3.length);
+
+console.log(Array1[0])
+console.log(Array2[1])
+console.log(Array3[2])
+
+console.log(Array1.includes(22));
+console.log(Array3.includes('Tudo bem?'));
